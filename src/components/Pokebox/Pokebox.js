9fa426619bb0d0a20cae744829b4caaa2183ebb4
Esben-Andreas-Madsen <316460@via.dk>
2023-04-20 13:19:47 +0200

    Router under control

--- src/components/Pokebox/Pokebox.js
+++ src/components/Pokebox/Pokebox.js
@@ -1,135 +1,52 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import styles from "./pokebox.module.css";
 
 function Pokebox() {
-  let [pokemon, setPokemon] = useState(null);
-  let [page, setPage] = useState(0);
-
-  useEffect(() => {
-    async function fetchData() {
-      let response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=20");
-      let data = await response.json();
-      let promises = data.results.map(async (result) => {
-        let pokemonResponse = await fetch(result.url);
-        return pokemonResponse.json();
-      });
-      const pokemonData = await Promise.all(promises);
-      setPokemon(pokemonData);
-    }
-
-    fetchData();
-  }, [page]);
-
-  if (!pokemon) {
-    return <div>Loading...</div>;
-  }
-
-  return (
-    <div className="poke-container">
-      {pokemon.map((pkmn) => (
-        <div key={pkmn.id} className={styles.pokebox}>
-          <p>#{pkmn.id}</p>
-          <p>{pkmn.name}</p>
-          <p>
-            Type: {pkmn.types[0].type.name} {pkmn.types[1]?.type.name}
-          </p>
-          <img src={pkmn.sprites.front_default} alt="pokemon" />
-        </div>
-      ))}
-    </div>
-  );
-}
-
-export default Pokebox;
-
-
-/* import { useState, useEffect } from "react";
-import styles from "./pokebox.module.css";
-
-function Pokebox() {
-  let [pokemon, setPokemon] = useState(null);
-  let [page, setPage] = useState(0);
-
-  useEffect(() => {
-    let pokemonsData = [];
-
-    async function fetchData() {
-      await fetch("https://pokeapi.co/api/v2/pokemon?limit=" + 20)
-        .then((response) => response.json())
-        .then(function (allpokemonurl) {
-          allpokemonurl.results.forEach(function (pokemonurl) {
-            fetchPokemonData(pokemonurl);
-          });
-        });
-
-      async function fetchPokemonData(pokemonurl) {
-        await fetch(pokemonurl.url)
-          .then((response) => response.json())
-          .then((pkmn) => pokemonsData.push(pkmn));
-      }
-      setPokemon(pokemonsData);
-    }
-
-    fetchData();
-    setPage(2);
-    console.log(pokemon);
-  }, [page]);
-  
-
-  return (
-    <>
-      {pokemon.map((pkmn) => (
-        <div key={pkmn.id}>
-          <p>#{pkmn.id}</p>
-          <p>{pkmn.name}</p>
-          <p>
-            Type: {pkmn.types[0].type.name} {pkmn.types[1]?.type.name}
-          </p>
-          <img src={pkmn.sprites.front_default} alt="pokemon" />
-        </div>
-      ))}
-    </>
-  );
-}
-
-export default Pokebox; */
-
-
-
-
-
-/* import { useState, useEffect } from "react";
-import styles from "./pokebox.module.css";
-
-function Pokebox() {
-  const [pokemon, setPokemon] = useState(null);
+  const [pokemon, setPokemon] = useState([]);
+  const [page, setPage] = useState(0);
 
   useEffect(() => {
     async function fetchData() {
       const response = await fetch(
-        "https://pokeapi.co/api/v2/pokemon/1"
-        );
-
+        `https://pokeapi.co/api/v2/pokemon?offset=${page}&limit=20`
+      );
       const data = await response.json();
-      setPokemon(data);
+      const pokemonsData = await Promise.all(
+        data.results.map(async (pokemonurl) => {
+          const response = await fetch(pokemonurl.url);
+          return response.json();
+        })
+      );
+      setPokemon(pokemonsData);
+      console.log(pokemonsData);
     }
-    fetchData();
-  }, []);
 
-  return pokemon ? (
-    <div className={styles.pokebox}>
-      <div>
-        <p>#{pokemon.id}</p>
-        <p>{pokemon.name}</p>
-        <p>
-          Type: {pokemon.types[0].type.name} {pokemon.types[1]?.type.name}
-        </p>
+    fetchData();
+  }, [page]);
+
+  function handleClick() {
+    setPage((prevPage) => prevPage + 20);
+  }
+
+  return (
+    <>
+      <div className={styles.button}>
+        <button onClick={handleClick}>Next Page</button>
       </div>
-      <img src={pokemon.sprites.front_default} alt="pokemon" />
-    </div>
-  ) : (
-    <p>Loading...</p>
+      <div className={styles.pokemonContainer}>
+        {pokemon.map((pkmn) => (
+          <div key={pkmn.id} className={styles.pokebox}>
+            <p>#{pkmn.id}</p>
+            <p>{pkmn.name}</p>
+            <p>
+              Type: {pkmn.types[0].type.name} {pkmn.types[1]?.type.name}
+            </p>
+            <img src={pkmn.sprites.front_default} alt="pokemon" />
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
 
-export default Pokebox; */
+export default Pokebox;
