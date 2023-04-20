92e08d59c256b9935ca77bb464453de8ce6b57fe
Raabeen Parameswaran <95747090+RaaBeeN02@users.noreply.github.com>
2023-04-20 12:32:02 +0200

    Style godkendt indtil videre

--- src/components/Pokebox/Pokebox.js
+++ src/components/Pokebox/Pokebox.js
@@ -1,42 +1,110 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import styles from "./pokebox.module.css";
 
 function Pokebox() {
-  let [pokemon, setPokemon] = useState(null);
-  let [page, setPage] = useState(0);
+  const [pokemon, setPokemon] = useState([]);
+  const [page, setPage] = useState(0);
 
   useEffect(() => {
     async function fetchData() {
-      let response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=20");
-      let data = await response.json();
-      let promises = data.results.map(async (result) => {
-        let pokemonResponse = await fetch(result.url);
-        return pokemonResponse.json();
-      });
-      const pokemonData = await Promise.all(promises);
-      setPokemon(pokemonData);
+      const response = await fetch(
+        `https://pokeapi.co/api/v2/pokemon?offset=${page}&limit=20`
+      );
+      const data = await response.json();
+      const pokemonsData = await Promise.all(
+        data.results.map(async (pokemonurl) => {
+          const response = await fetch(pokemonurl.url);
+          return response.json();
+        })
+      );
+      setPokemon(pokemonsData);
     }
 
     fetchData();
   }, [page]);
 
-  if (!pokemon) {
-    return <div>Loading...</div>;
+  function handleNextPage() {
+    setPage((prevPage) => prevPage + 20);
+  }
+
+  function handlePrevPage() {
+    setPage((prevPage) => Math.max(0, prevPage - 20));
+  }
+
+  function getTypeColor(typeName) {
+    switch (typeName) {
+      case "fire":
+        return "#FF9C54";
+      case "water":
+        return "#5CAFFF";
+      case "grass":
+        return "#9BCF9B";
+      case "electric":
+        return "#FFE24C";
+      case "ice":
+        return "#7CD4FF";
+      case "fighting":
+        return "#FF6B6B";
+      case "poison":
+        return "#AA5DA1";
+      case "ground":
+        return "#D6B55F";
+      case "flying":
+        return "#A891FF";
+      case "psychic":
+        return "#FF6B94";
+      case "bug":
+        return "#95A43B";
+      case "rock":
+        return "#C9BB8A";
+      case "ghost":
+        return "#6D68D6";
+      case "dragon":
+        return "#8B77FF";
+      case "dark":
+        return "#6E584C";
+      case "steel":
+        return "#C9C9C9";
+      case "fairy":
+        return "#FFA1E8";
+      default:
+        return "#EDEDED";
+    }
   }
 
   return (
-    <div className="poke-container">
-      {pokemon.map((pkmn) => (
-        <div key={pkmn.id} className={styles.pokebox}>
-          <p>#{pkmn.id}</p>
-          <p>{pkmn.name}</p>
-          <p>
-            Type: {pkmn.types[0].type.name} {pkmn.types[1]?.type.name}
-          </p>
-          <img src={pkmn.sprites.front_default} alt="pokemon" />
-        </div>
-      ))}
-    </div>
+    <>
+      <div className={styles.buttonContainer}>
+        <button onClick={handlePrevPage} disabled={page === 0}>
+          Previous Page
+        </button>
+        <button onClick={handleNextPage}>Next Page</button>
+      </div>
+      <div className={styles.pokemonContainer}>
+        {pokemon.map((pkmn, index) => (
+          <div
+            key={pkmn.id}
+            className={styles.pokebox}
+            style={{
+              backgroundColor: getTypeColor(pkmn.types[0].type.name),
+              border: `2px solid ${getTypeColor(pkmn.types[0].type.name)}`,
+            }}
+          >
+            <div className={styles.pokemonNumber}>#{page + index + 1}</div>
+            <div className={styles.pokemonName}>
+  {pkmn.name}
+  <br />
+  {pkmn.types.map((type) => type.type.name).join(", ")}
+</div>
+            <div className={styles.pokemonImageContainer}>
+              <img src={pkmn.sprites.front_default} alt={pkmn.name}
+                className={styles.pokemonImage}
+              />
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
 
